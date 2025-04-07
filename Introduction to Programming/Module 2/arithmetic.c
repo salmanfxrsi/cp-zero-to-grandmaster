599b3c1d6935ba6126b0063dfa455f3bca99cdb3
salmanfxrsi <salmanxprivate@gmail.com>
2025-04-07 23:35:11 +0600

    Arithmetic Operators

---
+++ Introduction to Programming/Module 2/arithmetic.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    float a = 10;
+    int b = 6;
+
+    int sum = a + b; // Summation
+    printf("Summation Is %d\n", sum);
+
+    int sub = a - b; // Subtraction
+    printf("Subtraction Is %d\n", sub);
+
+    int mul = a * b; // Multiplication
+    printf("Multiplication Is %d\n", mul);
+
+    float div = a / b; // Division
+    printf("Division Is %f\n", div);
+
+    return 0;
+}
