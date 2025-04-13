5629d8364d43a2044ef7a98591fa125ceb461161
salmanfxrsi <salmanxprivate@gmail.com>
2025-04-13 13:14:53 +0600

    First Digit

---
+++ Introduction to Programming/Module 5/first_digit.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main() 
+{
+    int X;
+    scanf("%d", &X);
+
+    while (X >= 10)
+    {
+        X = X / 10;
+    }
+
+    if(X % 2 == 0){
+        printf("EVEN\n");
+    }else{
+        printf("ODD\n");
+    }
+    
+
+    return 0;
+}
