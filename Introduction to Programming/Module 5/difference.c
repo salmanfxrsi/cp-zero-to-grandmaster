00e0a2d27a2c3b7f6b07e04232c9979e5100169c
salmanfxrsi <salmanxprivate@gmail.com>
2025-04-13 19:52:10 +0600

    Difference

---
+++ Introduction to Programming/Module 5/difference.c
@@ -0,0 +1,12 @@
+
+#include<stdio.h>
+ 
+int main() 
+{
+    long long X, A, B, C, D;
+    scanf("%lld %lld %lld %lld", &A, &B, &C, &D);
+ 
+    printf("Difference = %lld\n", (A *B) - (C * D));
+ 
+    return 0;
+}
