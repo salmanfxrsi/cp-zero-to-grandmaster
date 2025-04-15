221f0fe4a445b1864d81f91260152071b74894bd
salmanfxrsi <salmanxprivate@gmail.com>
2025-04-15 10:46:40 +0600

    Digits

--- Introduction to Programming/Module 6/Q_Digits.c
+++ Introduction to Programming/Module 6/Q_Digits.c
@@ -2,13 +2,23 @@
 
 int main() 
 {
-    int total_input = 0;
-    int input;
-
-    for(int counter = 0; counter <= total_input; counter++)
+    int T;
+    scanf("%d\n", &T);
+    
+    for(int counter = 1; counter <= T; counter++)
     {
+        int N;
+        scanf("%d\n", &N);
         
+        do
+        {
+            printf("%d ", N%10);
+            N /= 10; 
+        }while(N != 0);
+
+        printf("\n");
     }
+
 
     return 0;
 }
