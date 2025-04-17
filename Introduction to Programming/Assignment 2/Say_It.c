b33090359dc9ef172d7211fbc33d674a042af374
salmanfxrsi <salmanxprivate@gmail.com>
2025-04-17 23:30:38 +0600

    Say It

---
+++ Introduction to Programming/Assignment 2/Say_It.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() 
+{
+    int N;
+    scanf("%d", &N);
+
+    for(int counter = 1; counter <= N; counter++)
+    {
+        printf("%d. I Want More Assignments\n", counter);
+    }
+
+    return 0;
+}
