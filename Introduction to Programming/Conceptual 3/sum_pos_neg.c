d1152b3452d035f9d4a84122f00ceab9827cc758
salmanfxrsi <salmanxprivate@gmail.com>
2025-04-18 18:11:35 +0600

    Watermelon

---
+++ Introduction to Programming/Conceptual 3/sum_pos_neg.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main() 
+{
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    
+    for(int counter = 0; counter < n; counter++)
+    {
+        scanf("%d", &arr[counter]);
+    }
+
+    int  sum_pos = 0, sum_neg = 0;
+    
+    
+
+    return 0;
+}
