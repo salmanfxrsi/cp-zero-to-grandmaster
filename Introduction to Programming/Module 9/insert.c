f46bad51648ff704cf3272c365dcea4168e20dc1
salmanfxrsi <salmanxprivate@gmail.com>
2025-04-23 16:11:28 +0600

    Insert a value in an array idea

---
+++ Introduction to Programming/Module 9/insert.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main() 
+{
+    int n;
+    scanf("%d", &n);
+
+    int arr[n+1];
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    int idx, val;
+    
+    return 0;
+}
