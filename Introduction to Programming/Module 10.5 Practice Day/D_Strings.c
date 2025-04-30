880aef9190f0d7cf81cc7c507ad93d67bc12a657
salmanfxrsi <salmanxprivate@gmail.com>
2025-04-30 23:09:18 +0600

    Strings

--- Introduction to Programming/Module 10.5 Practice Day/D_Strings.c
+++ Introduction to Programming/Module 10.5 Practice Day/D_Strings.c
@@ -2,7 +2,23 @@
 
 int main() 
 {
-    int A;
+    char str1[11], str2[11];
+    scanf("%s %s", &str1, &str2);
+
+    int str1_length = 0;
+    int str2_length = 0;
+
+    for(int i = 0; str1[i] != '\0'; i++) str1_length++;
+    for(int i = 0; str2[i] != '\0'; i++) str2_length++;
+
+    printf("%d %d\n", str1_length, str2_length);
+    printf("%s%s\n", str1, str2);
+
+    int temp = str1[0];
+    str1[0] = str2[0];
+    str2[0] = temp;
+
+    printf("%s %s\n", str1, str2);
 
     return 0;
 }
