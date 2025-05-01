e137fcc51549afc1a596955b09a0649cd7be7e71
salmanfxrsi <salmanxprivate@gmail.com>
2025-05-02 00:17:27 +0600

    feat: learn string copy concept in C

---
+++ Introduction to Programming/Module 11/string_copy.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+
+int main() 
+{
+    char str1[101], str2[101];
+    scanf("%s %s", &str1, &str2);
+
+    for(int i = 0; i <= strlen(str2); i++)
+    {
+        str1[i] = str2[i];
+    }
+
+    printf("%s %s\n", str1, str2);
+
+    return 0;
+}
