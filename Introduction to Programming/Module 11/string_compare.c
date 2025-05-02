7893db88dbca0e25f76c25c3a21332e62764da0f
salmanfxrsi <salmanxprivate@gmail.com>
2025-05-02 12:47:06 +0600

    feat: learn manual string comparison using character-by-character logic

--- Introduction to Programming/Module 11/string_compare.c
+++ Introduction to Programming/Module 11/string_compare.c
@@ -7,5 +7,41 @@ int main()
     char a[101], b[101];
     scanf("%s %s", a, b);
 
+    int i = 0;
+    while (1)
+    {
+        if(a[i] == '\0' && b[i] == '\0') 
+        {
+            printf("Equal\n");
+            break;
+        }
+        else if(a[i] == '\0')
+        {
+            printf("a is smaller");
+            break;
+        }
+        else if(b[i] == '\0')
+        {
+            printf("b is smaller");
+            break;
+        }
+        else if(a[i] < b[i])
+        {
+            printf("a is smaller");
+            break;
+        }
+        else if(b[i] < a[i])
+        {
+            printf("b is smaller");
+            break;
+        }
+        else if(a[i] == a[i])
+        {
+            i++;
+        }
+
+    }
+    
+
     return 0;
 }
