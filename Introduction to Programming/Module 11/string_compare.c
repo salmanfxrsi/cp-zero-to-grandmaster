9097b827d658e56fa5da972ee511a2174cff1653
salmanfxrsi <salmanxprivate@gmail.com>
2025-05-02 10:53:36 +0600

    feat: learned about lexicographical comparison

---
+++ Introduction to Programming/Module 11/string_compare.c
@@ -0,0 +1,11 @@
+// Lexicographical comparison is the process of comparing two strings based on the order of characters, similar to how words are arranged in a dictionary.
+
+#include<stdio.h>
+
+int main() 
+{
+    char a[101], b[101];
+    scanf("%s %s", a, b);
+
+    return 0;
+}
