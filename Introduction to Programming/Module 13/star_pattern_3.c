1e2991bfb37ca71833ae2eb3140f76193ad00adb
salmanfxrsi <salmanxprivate@gmail.com>
2025-05-04 05:44:00 +0600

    feat: learn right-aligned triangle pattern in C

---
+++ Introduction to Programming/Module 13/star_pattern_3.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main() 
+{
+    int n;
+    scanf("%d", &n);
+
+    int star = 1;
+    int space = n - 1;
+
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 1; j <= space; j++)
+        {
+            printf(" ");
+        }
+        space--;
+
+        for(int k = 1; k <= star; k++)
+        {
+            printf("*");
+        }
+        star++;
+
+        printf("\n");
+    }
+
+    return 0;
+}
