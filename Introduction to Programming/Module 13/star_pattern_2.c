35114b4d53378f490cb22dc5dbfc516bfd755c82
salmanfxrsi <salmanxprivate@gmail.com>
2025-05-04 00:23:30 +0600

    chore: practiced Star Pattern 2 in C

--- Introduction to Programming/Module 13/star_pattern_2.c
+++ Introduction to Programming/Module 13/star_pattern_2.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main()
+int main() 
 {
     int n;
     scanf("%d", &n);
 
-    for (int i = 1; i <= n; i++)
+    for(int i = n; i >= 1; i--)
     {
-        for (int j = 1; j <= i; j++)
+        for(int j = i; j >= 1; j--) 
         {
             printf("*");
         }
