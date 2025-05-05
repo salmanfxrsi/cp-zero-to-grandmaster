c790f8168fccc9faeb0847a924a4f8d5aa72afe9
salmanfxrsi <salmanxprivate@gmail.com>
2025-05-06 02:24:52 +0600

    feat: solve H. Sorting using bubble sort without built-in functions

---
+++ Introduction to Programming/Module 14.5 Practice Day/H_Sorting.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+
+    int arr[N];
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = i + 1; j < N; j++)
+        {
+            if(arr[i] > arr[j])
+            {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    for(int i = 0; i < N; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
