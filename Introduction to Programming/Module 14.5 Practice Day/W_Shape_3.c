8d119c2acd62d0d704250769411c49a54dbd00b6
salmanfxrsi <salmanxprivate@gmail.com>
2025-05-06 12:22:22 +0600

    feat: implement diamond pattern printer for Shape3 problem

--- Introduction to Programming/Module 14.5 Practice Day/W_Shape_3.c
+++ Introduction to Programming/Module 14.5 Practice Day/W_Shape_3.c
@@ -5,11 +5,42 @@ int main()
     int N;  
     scanf("%d", &N);
 
-    int star = 1, gap = 0;
+    int star = 1, gap = N - 1;
 
-    for(int i = 0; i < (N * 2); i++)
+    for(int i = 0; i < N; i++)
     {
-        
+        for(int j = 0; j < gap; j++)
+        {
+            printf(" ");
+        }
+        gap--;
+
+        for(int k = 0; k < star; k++)
+        {
+            printf("*");
+        }
+        star += 2;
+
+        printf("\n");
+    }
+
+    star -= 2;
+
+    for(int i = 0; i < N; i++)
+    {
+        for(int j = 0; j < i; j++)
+        {
+            printf(" ");
+        }
+        gap--;
+
+        for(int k = 0; k < star; k++)
+        {
+            printf("*");
+        }
+        star -= 2;
+
+        printf("\n");
     }
 
     return 0;
