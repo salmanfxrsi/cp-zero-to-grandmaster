eec01455d63a9cf7e7391aa4edadab2a1246899a
salmanfxrsi <salmanxprivate@gmail.com>
2025-05-06 20:43:42 +0600

    feat: learn pointer declaration, assignment, and address printing

---
+++ Introduction to Programming/Module 15/pointers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() 
+{
+    int x;
+    scanf("%d", &x);
+    printf("%d\n", x);
+    printf("%p\n", &x);
+
+    int* ptr;
+    ptr = &x;
+    printf("%p\n", ptr);
+    printf("%p\n", &ptr);
+    
+    return 0;
+}
