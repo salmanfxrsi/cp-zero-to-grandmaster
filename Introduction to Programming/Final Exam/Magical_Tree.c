feadf3309f3abf2a758dbc93ef96cc8aa257b56f
salmanfxrsi <salmanxprivate@gmail.com>
2025-05-18 16:50:52 +0600

    feat: print Christmas tree of size N with variable trunk based on input

--- Introduction to Programming/Final Exam/Magical_Tree.c
+++ Introduction to Programming/Final Exam/Magical_Tree.c
@@ -5,24 +5,38 @@ int main()
     int N;
     scanf("%d", &N);
     
-    int star = 1, gap = 5 + (3 / 2);
+    int gap = 5 + (N / 2) , star = 1;
 
-    for(int i = 1; i <= 5 + N; i++)
+    for(int i = 1; i <= 6 + (N / 2); i++)
     {
-        for(int j = gap; j > 1; j--)
+        for(int j = 1; j <= gap; j++)
         {
             printf(" ");
         }
         gap--;
-        
+
         for(int j = 1; j <= star; j++)
         {
             printf("*");
         }
         star += 2;
-        
-        printf("\n");
 
+        printf("\n");
+    }
+
+    for(int i = 1; i <= 5; i++)
+    {
+        for(int j = 1; j <= 5; j++)
+        {
+            printf(" ");
+        }
+
+        for(int j = 1; j <= N; j++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
     }
 
     return 0;
