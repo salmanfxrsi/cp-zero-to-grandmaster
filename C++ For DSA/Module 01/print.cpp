ff72a0bb092f6373e4950d72662542834988c8b3
salmanfxrsi <salmanxprivate@gmail.com>
2025-05-25 05:52:30 +0600

    chore: learned how to print output using C++ cout

---
+++ C++ For DSA/Module 01/print.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main()
+{
+    int x = 100;
+    char c = 'S';
+    double d = 34.56;
+    std :: cout << x << " " << c << " " << d << std :: endl << "Hello, World!" << std :: endl << "The Number is" << " " << x << std :: endl;
+
+    return 0;
+}
