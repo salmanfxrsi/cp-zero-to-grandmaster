1a1a0e82cfa51205715c4eae88fbaeb4b9e290a1
salmanfxrsi <salmanxprivate@gmail.com>
2025-05-25 06:38:00 +0600

    chore: learned if-else statements and ternary operator in C++

---
+++ C++ For DSA/Module 01/if_else.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+    int num = 3;
+    if(num % 2 == 0)
+    {
+        cout << "Even\n";
+    }
+    else
+    {
+        cout << "Odd\n";
+    }
+
+    return 0;
+}
---
+++ C++ For DSA/Module 01/ternary.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num = 3;
+    num % 2 == 0 ? cout << "Even\n" : cout << "Odd\n";
+
+    return 0;
+}
