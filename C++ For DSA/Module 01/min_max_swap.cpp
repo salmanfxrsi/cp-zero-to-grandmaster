a30a13055dd6845ce8f3d3fc06be14625f373a5c
salmanfxrsi <salmanxprivate@gmail.com>
2025-05-25 11:13:39 +0600

    chore: learned min, max, and swap functions from <algorithm> header in C++

---
+++ C++ For DSA/Module 01/min_max_swap.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main() 
+{
+    // Min Function
+    cout << min({2, 4, 4, 7, 5, 3, 3 , 1, 8, 4}) << endl;
+
+    // Max Function
+    cout << max({2, 4, 4, 7, 5, 3, 3 , 1, 8, 4}) << endl;
+    
+    // Swap Function
+    int a = 5, b = 10;
+    swap(a, b);
+    cout << a << " " << b << endl;
+
+    return 0;
+}
