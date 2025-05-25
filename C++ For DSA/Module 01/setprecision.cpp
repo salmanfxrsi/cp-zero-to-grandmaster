1ff6041ae9c998fcc099e0d702b6070723535279
salmanfxrsi <salmanxprivate@gmail.com>
2025-05-25 06:27:59 +0600

    chore: learned about EOF and setprecision in C++

---
+++ C++ For DSA/Module 01/setprecision.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+    double num;
+    cin >> num;
+    cout << fixed << setprecision(2) << num << endl;
+
+    return 0;
+}
