11aa1ac5fa8a93261efc251e219b66b69f77b6fd
salmanfxrsi <salmanxprivate@gmail.com>
2025-05-31 13:36:31 +0600

    feat: learn the need for dynamic objects and explore implementation in code

---
+++ C++ For DSA/Module 03/dynamic_object.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+public:
+    int roll;
+    int cls;
+    double gpa;
+
+    Student(int roll = 0, int cls = 0, double gpa = 0.00)
+    {
+        // (*this).roll = roll;
+        this->roll = roll;
+        this->cls = cls;
+        this->gpa = gpa;
+    }
+};
+
+Student * fun()
+{
+    Student karim(6, 12);
+    Student * p = &karim;
+    return p;
+}
+
+int main() 
+{
+    Student salman(1, 12, 5.00);
+    Student * karim = new Student(10, 12, 3.78);
+    cout << salman.roll << " " << salman.cls << " " << salman.gpa << endl;
+    cout << karim->roll << " " << karim->cls << " " << karim->gpa << endl;
+    return 0;
+}
