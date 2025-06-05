8c167e84837919145c3d167a84382f51c14f1564
salmanfxrsi <salmanxprivate@gmail.com>
2025-06-05 20:18:56 +0600

    chore: learn how to handle string input with spaces

---
+++ C++ For DSA/Module 05/string_input_with_space.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+    string s;
+    // cin >> s;
+    // cin.getline(s, 100);
+    cin.ignore();  
+    getline(cin, s);
+    cout << x << endl;
+    cout << s << endl;
+
+    return 0;
+}
