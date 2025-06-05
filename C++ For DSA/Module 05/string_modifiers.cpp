99bc7d191d0c02d8f10febe92469b81465ecbc1b
salmanfxrsi <salmanxprivate@gmail.com>
2025-06-05 17:39:54 +0600

    chore: learn and practice string modifier functions in C++

---
+++ C++ For DSA/Module 05/string_modifiers.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s = "Hello, World!";
+    // s[2] = 'X';
+    // string s2 = "Hi!";
+    // s += s2;
+    // s.append(s2);
+    // s.push_back('X');
+    // s += "X";
+    // s.pop_back();
+    // s = "Hey, World!";
+    // s = s2;
+    // s.assign(s2);
+    // s.erase(4, 1);
+    // s.replace(0, 3, "Greetings");
+    // s.insert(5, "Salman's ");
+    cout << s << endl;
+
+    return 0;
+}
