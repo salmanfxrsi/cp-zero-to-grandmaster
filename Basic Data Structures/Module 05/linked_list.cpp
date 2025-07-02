5cded8bf8c6472824a66562a36a948bd7aff2ed9
salmanfxrsi <salmanxprivate@gmail.com>
2025-07-02 21:18:06 +0600

    chore: Learn that arrays have a fixed size

---
+++ Basic Data Structures/Module 05/linked_list.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> v = {1, 3, 3, 4, 5};
+    v.push_back(100);
+    cout << (int)&v[4] << " " << (int)&v[5];
+    return 0;
+}
