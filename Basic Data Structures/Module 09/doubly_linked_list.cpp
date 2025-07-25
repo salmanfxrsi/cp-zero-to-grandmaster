23c742101976916af427dbc236c0513997ca3823
salmanfxrsi <salmanxprivate@gmail.com>
2025-07-25 11:40:33 +0600

    chore: studied and practiced doubly linked list implementation and traversal

---
+++ Basic Data Structures/Module 09/doubly_linked_list.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *prev;
+    Node *next;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+};
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *tail = new Node(30);
+
+    head->next = a;
+    a->prev = head;
+    
+    a->next = tail;
+    tail->prev = a;
+
+    return 0;
+}
