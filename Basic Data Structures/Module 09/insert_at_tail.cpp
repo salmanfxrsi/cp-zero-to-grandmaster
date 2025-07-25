c7fbd6583222fc617025415f7765476ff9c764b8
salmanfxrsi <salmanxprivate@gmail.com>
2025-07-25 14:47:14 +0600

    chore: learned and implemented insert-at-tail logic in doubly linked list with head and tail handling

--- Basic Data Structures/Module 09/insert_at_tail.cpp
+++ Basic Data Structures/Module 09/insert_at_tail.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
 class Node
@@ -37,7 +37,7 @@ void print_backward(Node *tail)
     }
 }
 
-void insert_at_head(Node *&head, Node *&tail, int val)
+void insert_at_tail(Node *&head, Node *&tail, int val)
 {
     Node *newnode = new Node(val);
     if(head == NULL)
@@ -46,26 +46,26 @@ void insert_at_head(Node *&head, Node *&tail, int val)
         tail = newnode;
         return;
     }
-    newnode->next = head;
-    head->prev = newnode;
-    head = newnode;
-}
+    newnode->prev = tail;
+    tail->next = newnode;
+    tail = newnode;
+} 
 
 int main()
 {
-    Node *head = new Node(10);
-    Node *a = new Node(20);
-    Node *tail = new Node(30);
+    Node *head = NULL;
+    // Node *a = new Node(20);
+    Node *tail = NULL;
 
-    head->next = a;
-    a->prev = head;
+    // head->next = a;
+    // a->prev = head;
 
-    a->next = tail;
-    tail->prev = a;
+    // a->next = tail;
+    // tail->prev = a;
 
-    insert_at_head(head, tail, 100);
-    insert_at_head(head, tail, 200);
-    insert_at_head(head, tail, 300);
+    insert_at_tail(head, tail, 40);
+    insert_at_tail(head, tail, 50);
+    insert_at_tail(head, tail, 60);
 
     print_forward(head);
     return 0;
