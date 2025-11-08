66894a4d3d5dcecc33a54b633c17c51aef714993
salmanfxrsi <salmanxprivate@gmail.com>
2025-11-08 21:04:57 +0600

    chore: learned how to print a square number pattern using nested loops in C++

---
+++ DSA Series By Shradha Ma'am/Patterns/square_pattern.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int counter = 1;
+
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            cout << counter << " ";
+            counter++;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
