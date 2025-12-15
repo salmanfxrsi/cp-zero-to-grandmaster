427d0997628dac5e8094fdf799a1d97e4edd77e1
Salman Farsi <salmanfarsi9005@gmail.com>
2025-12-15 11:07:30 +0600

    chore: update sum of digits implementation

---
+++ DSA Series By Shradha Ma'am/Functions/sum_of_digit_of_number.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+
+int sum_of_digit(int n)
+{
+    int sum = 0;
+    while(n > 0){
+        sum += n % 10;
+        n = n / 10;
+    }
+
+    return sum;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    cout << sum_of_digit(n);
+
+    return 0;
+}
