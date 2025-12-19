88ca290fc205a7b47af4d8e8507250fff3ec1188
salmanfxrsi <salmanxprivate@gmail.com>
2025-12-19 22:47:06 +0600

    chore: learn difference between local and global scope

---
+++ DSA Series By Shradha Ma'am/Bitwise Operators, Data Type Modifiers & More/scope.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int counter = 0;
+
+    if (counter < 0)
+        counter++;
+    else
+        counter--;
+
+    for (int i = 1; i <= 100; i++)
+    {
+        cout << "hello programmer salman!";
+    }
+
+    return 0;
+}
