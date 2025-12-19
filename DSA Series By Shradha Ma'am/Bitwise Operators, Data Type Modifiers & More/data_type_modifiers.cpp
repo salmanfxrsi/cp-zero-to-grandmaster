6bb7d313658bd9dc47949be5312ee8b14eec3725
salmanfxrsi <salmanxprivate@gmail.com>
2025-12-19 23:04:06 +0600

    chore: learn and apply data type modifiers

---
+++ DSA Series By Shradha Ma'am/Bitwise Operators, Data Type Modifiers & More/data_type_modifiers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    unsigned x = -10;
+
+    cout << x << endl;
+    cout << sizeof(int) << endl;
+    cout << sizeof(long int) << endl;
+    cout << sizeof(long long int) << endl;
+    cout << sizeof(short int) << endl;
+
+    return 0;
+}
