77631fbbd56e95a83e8e0d5731178642427564ab
Salman Farsi <salmanfarsi9005@gmail.com>
2026-01-05 22:41:40 +0600

    chore: understand O(n) time complexity

---
+++ DSA Series By Shradha Ma'am/Time & Space Complexity/O(n).cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n; 
+    int fact = 0;
+    for(int i = 1; i <= n; i++) 
+    {
+        fact *= i; // O(n)
+    }
+    cout << fact;
+    return 0;
+}
