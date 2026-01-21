25b19a823df5baf4864ae79dce507cb52ca4fa0b
Salman Farsi <salmanfarsi9005@gmail.com>
2026-01-22 02:08:06 +0600

    chore: learn kadane algorithm

---
+++ DSA Series By Shradha Ma'am/Kadane's Algorithm | Maximum Subarray Sum/kadane_algorithm.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <climits>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n = 7;
+    int arr[7] = {3, -4, 5, 4, -1, 7, -8};
+
+    int currSum = 0, maxSum = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        currSum += arr[i];
+        maxSum = max(currSum, maxSum);
+        if (currSum < 0)
+            currSum = 0;
+    }
+
+    cout << maxSum;
+    return 0;
+}
