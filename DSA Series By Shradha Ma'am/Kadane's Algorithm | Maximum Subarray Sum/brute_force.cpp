267915c06ad6fb9df4fdc63f29a4faa7c1368ca4
Salman Farsi <salmanfarsi9005@gmail.com>
2026-01-22 01:09:21 +0600

    chore: learn basics of subarray concept

--- DSA Series By Shradha Ma'am/Kadane's Algorithm | Maximum Subarray Sum/brute_force.cpp
+++ DSA Series By Shradha Ma'am/Kadane's Algorithm | Maximum Subarray Sum/brute_force.cpp
@@ -8,7 +8,15 @@ int main()
 
     for (int st = 0; st < n; st++)
     {
-        
+        for (int end = st; end < n; end++)
+        {
+            for (int i = st; i <= end; i++)
+            {
+                cout << arr[i];
+            }
+            cout << " ";
+        }
+        cout << endl;
     }
 
     return 0;
